0a579ab9ed3b130445fb85e5a5c5962de8b1c9db
NEDUNURIGANESH <113292508+NEDUNURIGANESH@users.noreply.github.com>
2023-04-02 16:57:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app=express();
 const bodyParser=require('body-parser');
 const https=require('https');
 app.use(bodyParser.urlencoded({extended:true}));
-//to make request with external server we use https module.(Note: we can use other modules like axios,got etc)
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");    
 });
 app.post("/",(req,res)=>{ 
-    console.log(req.body.city);
     const query=req.body.city;
     const appKey="a4c09b3b47ac209491a111356ab9f1c7";
     const unit="metric";
@@ -30,30 +28,5 @@ app.post("/",(req,res)=>{
         });
     });
 });
-//     const query=req.body.city;
-//     const appKey="a4c09b3b47ac209491a111356ab9f1c7";
-//     const unit="metrics";
-//     const url="https://api.openweathermap.org/data/2.5/forecast?q=" + query +"&appid=" + appKey +"&units=" + unit;
-//     https.get(url,(response)=>{
-//         console.log(response.statusCode);
-//         response.on("data",(data)=>{
-        
-//             const weatherData=JSON.parse(data);
-//             const temp=weatherData.list[1].main.temp;
-//             const weatherDescription=weatherData.list[1].weather[0].description;
-//             const icon=weatherData.list[1].weather[0].icon;
-//             const imageURL="https://openweathermap.org/img/wn/"+ icon +"@2x.png";
-//             //we can write many res.write() but there should be only res.send() otherwise server will throw error
-//             res.write("<h1>current temperature in "+ query+" is "+temp+" degree celsius</h1>");
-//             res.write("<h2>weather condition is "+weatherDescription+"</h2>")
-//             res.write("<img src="+ imageURL +">");
-//             //res.write("<h1>current temperature in Hyderabad is "+temp+" degree celsius</h1>");
-//             res.send();
-//         });
-//     });
-// });
-
-
-
     app.listen(3000);
 });
